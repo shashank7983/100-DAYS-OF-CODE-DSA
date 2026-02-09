e7ba418a5c03696a23bcbca449663f9a4c788692
shashank7983 <tiwarishashank7895@gmail.com>
2026-02-09 19:52:05 +0530

    DAY 8

---
+++ Q8.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int power(int a, int b){
+    if (b == 0){
+        return 1;
+    }
+    return a*power(a, b-1);
+}
+
+int main(){
+    int a,b;
+    printf("Enter the value of a and b: \n");
+    scanf("%d %d", &a, &b);
+
+    printf("%d", power(a,b));
+}
